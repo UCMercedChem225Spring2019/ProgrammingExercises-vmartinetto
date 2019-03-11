c787172efc5e596e8e58cb4eeffb3753d3c4357a
Vincent Martinetto <vmartinetto@ucmerced.edu>
2019-03-11 16:11:17 -0700

    commit

--- pgrm_02_01.f90
+++ pgrm_02_01.f90
@@ -28,9 +28,9 @@
       Read(IIn,*) NDim
       Allocate(Array_Input(NDim*NDim),Matrix(NDim,NDim))
 !
-! *************************************************************************
-! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
-! *************************************************************************
+      do i = 1,NDim*NDim
+        Read (IIn,*) Array_Input(i)
+      end do
 !
 !
 !     Convert Array_Input to Matrix and print the matrix.
@@ -64,15 +64,53 @@
 !     Loop through the elements of AMatOut and fill them appropriately from
 !     Array_Input.
 !
+      k=1
+      do i = 1,N
+        do j = 1,M
+          AMatOut(j,i) = ArrayIn(k)
+          k = k+1
+        end do
+      end do
 !
-! *************************************************************************
-! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
-! *************************************************************************
+      return
+      end subroutine Packed2Matrix_ColumnWise
 !
+!
+!
+      Subroutine Print_Matrix_Full_Real(AMat,M,N)
+!
+!     This subroutine prints a real matrix that is fully dimension - i.e.,
+!     not stored in packed form. AMat is the matrix, which is dimensioned
+!     (M,N).
+!
+!     The output of this routine is sent to unit number 6 (set by the local
+!     parameter integer IOut).
+!
+!
+!     Variable Declarations
+!
+      implicit none
+      integer,intent(in)::M,N
+      real,dimension(M,N),intent(in)::AMat
+!
+!     Local variables
+      integer,parameter::IOut=6,NColumns=5
+      integer::i,j,IFirst,ILast
+!
+ 1000 Format(1x,A)
+ 2000 Format(5x,5(7x,I7))
+ 2010 Format(1x,I7,5F14.6)
+!
+      Do IFirst = 1,N,NColumns
+        ILast = Min(IFirst+NColumns-1,N)
+        write(IOut,2000) (i,i=IFirst,ILast)
+        Do i = 1,M
+          write(IOut,2010) i,(AMat(i,j),j=IFirst,ILast)
+        endDo
+      endDo
 !
       Return
-      End Subroutine Packed2Matrix_ColumnWise
-
+      End Subroutine Print_Matrix_Full_Real
 
       Subroutine Packed2Matrix_RowWise(M,N,ArrayIn,AMatOut)
 !
@@ -91,10 +129,14 @@
 !     Loop through the elements of AMatOut and fill them appropriately from
 !     Array_Input.
 !
-!
-! *************************************************************************
-! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
-! *************************************************************************
+      k=1
+      do i = 1,n
+        do j = 1,m
+          AMatOut(i,j) = ArrayIn(k)
+          k = k+1
+        end do
+      end do
+!     
 !
 !
       Return
